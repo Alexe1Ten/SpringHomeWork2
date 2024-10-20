c4e687e147a24a1148eaf37dfd5d8c398f0a883d
AlexeyTen <aten9180929937@gmail.com>
2024-10-20 20:53:07 +0300

    fix bugs

--- src/main/java/com/example/demo/repositories/UserRepository.java
+++ src/main/java/com/example/demo/repositories/UserRepository.java
@@ -1,12 +1,12 @@
 package com.example.demo.repositories;
 
-import com.example.demo.model.User;
-import com.example.demo.utils.UserMapper;
+import java.util.List;
+
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
+import com.example.demo.model.User;
 
     @Repository
     public class UserRepository {
@@ -19,17 +19,32 @@ import java.util.List;
 
 
 
+        // public User findUserById(Integer id) {
+        //     String sql = "SELECT * FROM userTable WHERE id=?";
+        //     RowMapper<User> userRowMapper = (r, i) -> {
+        //         User rowObject = new User();
+        //         rowObject.setId(r.getInt("id"));
+        //         rowObject.getFirstName();
+        //         rowObject.getLastName();
+        //         return rowObject;
+        //     };
+        //     return jdbcTemplate.query(sql, new Object[]{id}, new UserMapper())
+        //             .stream().findFirst().orElse(null);
+        // }
+
         public User findUserById(Integer id) {
             String sql = "SELECT * FROM userTable WHERE id=?";
+            
             RowMapper<User> userRowMapper = (r, i) -> {
                 User rowObject = new User();
                 rowObject.setId(r.getInt("id"));
-                rowObject.getFirstName();
-                rowObject.getLastName();
+                rowObject.setFirstName(r.getString("firstName"));
+                rowObject.setLastName(r.getString("lastName"));
                 return rowObject;
             };
-            return jdbcTemplate.query(sql, new Object[]{id}, new UserMapper())
-                    .stream().findFirst().orElse(null);
+            
+            // Используем queryForObject для получения одного результата
+            return jdbcTemplate.queryForObject(sql, userRowMapper, id);
         }
 
 
